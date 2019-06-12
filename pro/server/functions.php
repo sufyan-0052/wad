49a7729265972c0dfd959b5c9037691068644ff4
Sufyan-0052 <sufiam19@ucp.edu.pk>
2019-06-12 16:10:32 +0500

    Merge remote-tracking branch 'origin/master'
    #	css/main.css

--- pro/server/functions.php
+++ pro/server/functions.php
@@ -8,7 +8,7 @@ function getCats(){
     while($row = mysqli_fetch_assoc($getCatsResult)){
         $cat_id = $row['cat_id'];
         $cat_title = $row['cat_title'];
-        echo "<li><a class='nav-link'  href='#'>$cat_title</a></li>";
+        echo "<li><a class='nav-link'  href='index.php?pro_cat=$cat_id'>$cat_title</a></li>";
     }
 }
 function getBrands(){
@@ -18,9 +18,49 @@ function getBrands(){
     while($row = mysqli_fetch_assoc($getBrandsResult)){
         $brand_id = $row['brand_id'];
         $brand_title = $row['brand_title'];
-        echo "<li><a class='nav-link'  href=''#'>$brand_title</a></li>";
+        echo "<li><a class='nav-link'  href='index.php?pro_brand=$brand_id'>$brand_title</a></li>";
     }
 }
 
+function getPro(){
+    global $con;
+    $getProQuery='';
+    if(isset($_GET['pro_cat'])){
+        $cat_id=$_GET['pro_cat'];
+        $getProQuery=" select * from products where pro_cat ='$cat_id' ";
+    }
+    elseif(isset($_GET['pro_brand'])){
+        $brand_id=$_GET['pro_brand'];
+        $getProQuery=" select * from products where pro_brand ='$brand_id'";
+    }
+    elseif(isset($_GET['search'])){
+        $q=$_GET['search'];
+        $getProQuery=" select * from products where pro_Keywords like '%$q%' ";
+    }
+    else{
+        $getProQuery="select * from products";
+    }
+    $getProResult=mysqli_query($con,$getProQuery);
+    while($row=mysqli_fetch_assoc($getProResult)) {
+        $pro_title = $row['pro_title'];
+        $pro_price = $row['pro_price'];
+        $pro_image = $row['pro_image'];
+
+        echo "
+      
+      <div class='col-sm-6 col-md-4 col-lg-3 text-center product-summary'>
+                <h5 class='text-capitalize'> $pro_title </h5>
+                <img src='admin/product_images/$pro_image' >
+                <p> <b> $pro_price  </b> </p>
+                <a href='#' class='btn btn-info btn-sm'>Details</a>
+                <a href='#'>
+                    <button class='btn btn-primary btn-sm'>
+                        <i class='fas fa-cart-plus'></i> Add to Cart
+                    </button>
+                </a>
+            </div>
+    ";
+    }
+}
 
 
